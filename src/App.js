e036f37c9667292f43943b18252f2bd8c420a818
edwinisnow <edwin04@gmail.com>
2019-08-31 23:32:07 +0100

    State

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroname="Batman" >This is children props</Greet>
+      <Message />
+      {/* <Greet name="Bruce" heroname="Batman" >This is children props</Greet>
       <Greet name="Clark" heroname="Superman" ><button>Action</button></Greet>
       <Welcome name="Bruce" heroname="Batman" />
-      <Welcome name="Clark" heroname="Superman" />
+      <Welcome name="Clark" heroname="Superman"/> */}
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Greet = (props) => {
     return (<div>
-        <h1>Hello {props.name} a.k.a {props.heroname}</h1>
+        <h1>Functional Component {props.name} a.k.a {props.heroname}</h1>
         {props.children}
     </div>)
 }

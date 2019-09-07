beb1babd53b4d4ba69e74aff2e61eef6fbf2eb48
edwinisnow <edwin04@gmail.com>
2019-09-07 07:26:43 +0100

    Component Mounting Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -17,10 +17,17 @@ import Inline from './components/Inline';
 
 import './appStyles.css';
 import styles from './appStyles.module.css'
+import Form from './components/Form';
+import LifeCycleA from './components/LifeCycleA';
 
 function App() {
   return (
     <div className="App">
+
+      <LifeCycleA />
+
+      {/* <Form /> */}
+
       {/* <h1 className="error">Error</h1>
       <h1 className={styles.success}>Success</h1> */}
       {/* <Inline /> */}

90d28cf9c689a3505920a63eaab5d7a2305a66ba
edwinisnow <edwin04@gmail.com>
2019-09-09 20:19:47 +0100

    React Callback Ref

--- src/RefsDemo.js
+++ src/RefsDemo.js
@@ -4,11 +4,19 @@ class RefsDemo extends Component {
     constructor(props) {
         super(props)
         this.inputRef = React.createRef();
+        // old approach
+        this.cbRef = null;
+        this.setCbRef = element => {
+            this.cbRef = element
+        }
 
     }
     componentDidMount() {
-        console.log(this.inputRef)
-        this.inputRef.current.focus()
+        // console.log(this.inputRef)
+        // this.inputRef.current.focus()
+        if (this.cbRef) {
+            this.cbRef.focus()
+        }
     }
 
     clickHandler = () => {
@@ -19,6 +27,7 @@ class RefsDemo extends Component {
         return (
             <div>
                 <input ref={this.inputRef} type="text" />
+                <input ref={this.setCbRef} type="text" />
                 <button onClick={this.clickHandler}>Click</button>
             </div>
         )

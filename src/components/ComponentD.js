2aec418fc3a3dc2be88122d3e7f011cc9bab4298
edwinisnow <edwin04@gmail.com>
2019-09-15 23:04:53 +0100

    Context API contextType

--- src/components/ComponentD.js
+++ src/components/ComponentD.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import ComponentE from './ComponentE';
+import UserContext from './UserContext';
 
 class ComponentD extends Component {
+    static contextType = UserContext;
     render() {
         return (
             <div>
+                Component D context {this.context}
                 <ComponentE />
             </div>
         )
     }
 }
+
+// ComponentD.contextType = UserContext;
 
 export default ComponentD
